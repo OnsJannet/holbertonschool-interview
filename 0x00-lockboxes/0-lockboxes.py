48e81d9cfe5841d689fd1fdaffe1c05d238c9937
onsjannet <onsjannet@gmail.com>
2021-04-21 22:42:11 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,12 +3,12 @@
 
 def canUnlockAll(boxes):
     ''' determines if all the boxes can be opened '''
-    all_boxes = [0]
+        all_boxes = [0]
     for key in all_boxes:
         for boxes_kies in boxes[key]:
             if boxes_kies not in all_boxes:
-            	if boxes_kies < len(boxes):
-                	all_boxes.append(boxes_kies)
-    if len(all_boxes) is len(boxes):
+                if boxes_kies < len(boxes):
+                    all_boxes.append(boxes_kies)
+    if len(all_boxes) == len(boxes):
         return True
     return False
