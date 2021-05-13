c3680b1ebd714f7c54164efd08d24fd971867943
onsjannet <onsjannet@gmail.com>
2021-05-13 18:31:24 +0100

    minimum operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,15 +5,16 @@ in exactly n H characters in the file.
 
 """
 
+
 def minOperations(n):
-	operation_count = 0
-	number = 2
-	if type(n) is not int or n <= 1:
-		return operation_count
-	while n > 1:
-		if n % number == 0:
-			operation_count += number
-			n //= number			
-		else:
-			number += 1
-	return(operation_count)
+    operation_count = 0
+    number = 2
+    if type(n) is not int or n <= 1:
+        return operation_count
+    while n > 1:
+        if n % number == 0:
+            operation_count += number
+            n //= number
+        else:
+            number += 1
+    return(operation_count)
