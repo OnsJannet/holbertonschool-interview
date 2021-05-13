ddd18908b9448b5acac5da9de9a896673b28bebf
onsjannet <onsjannet@gmail.com>
2021-05-13 18:28:55 +0100

    minimum operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,6 +2,18 @@
 """
 calculates the fewest number of operations needed to result
 in exactly n H characters in the file.
-Copy All : copies
-Paste: pastes
+
 """
+
+def minOperations(n):
+	operation_count = 0
+	number = 2
+	if type(n) is not int or n <= 1:
+		return operation_count
+	while n > 1:
+		if n % number == 0:
+			operation_count += number
+			n //= number			
+		else:
+			number += 1
+	return(operation_count)
