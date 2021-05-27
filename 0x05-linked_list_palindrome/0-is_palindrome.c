82f6256b745be2df9bdf0d5a85f44e6538619bd5
onsjannet <onsjannet@gmail.com>
2021-05-27 20:03:08 +0100

    chmod

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome_1 - checks if a singly linked list is a palindrome.
+ *
+* @left: left side
+* @right: right side
+ * Return: 0 if it is not a palindrome 1 if it is a palindrome.
+ */
+
+int recursive_palindrome(listint_t **left, listint_t *right)
+
+{
+
+
+	if (right == NULL)
+		return (1);
+
+
+	int ispalindrome = recursive_palindrome(left, right->next);
+
+	if (ispalindrome == 0)
+		return (0);
+
+
+	int ispalindrome1 = (right->n == (*left)->n);
+
+
+	*left = (*left)->next;
+
+	return (ispalindrome1);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: Double pointer to head of list
+ * Return: 0 if it is not a palindrome 1 if it is a palindrome.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	return (recursive_palindrome(head, *head));
+
+}
