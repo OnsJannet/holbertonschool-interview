ca06f1e1f6a46c11d41cdec5199a22fe0aecf262
onsjannet <onsjannet@gmail.com>
2021-08-12 19:29:20 +0100

    STARTWARS

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// a script that prints all characters of a Star Wars movie
+
+const request = require('request');
+const movies = process.argv.slice(2);
+
+request.get(`https://swapi-api.hbtn.io/api/films/${movies}/`, async (_error, response, body) => {
+    for (const character of JSON.parse(body).characters) {
+    	const people = await new Promise((resolve, reject) => {
+    		request(character, (error, response, body) => {
+    			if (error) {
+    				reject(error);
+    			} else {
+    				resolve(JSON.parse(body).name);
+    			}
+    		});
+    	});
+    	console.log(people);
+    }
+});
