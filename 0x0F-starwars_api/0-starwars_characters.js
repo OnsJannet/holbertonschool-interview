0fabb48cf7bd179cbef981c6e64443a0a58706bd
onsjannet <onsjannet@gmail.com>
2021-08-12 19:34:27 +0100

    starwars API

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -6,16 +6,16 @@ const request = require('request');
 const movies = process.argv.slice(2);
 
 request.get(`https://swapi-api.hbtn.io/api/films/${movies}/`, async (_error, response, body) => {
-    for (const character of JSON.parse(body).characters) {
-    	const people = await new Promise((resolve, reject) => {
-    		request(character, (error, response, body) => {
-    			if (error) {
-    				reject(error);
-    			} else {
-    				resolve(JSON.parse(body).name);
-    			}
-    		});
-    	});
-    	console.log(people);
-    }
+  for (const character of JSON.parse(body).characters) {
+    const people = await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
+      });
+    });
+    console.log(people);
+  }
 });
