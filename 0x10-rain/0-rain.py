6896a1724b79b85907a025ff8a78e12201909dc4
onsjannet <onsjannet@gmail.com>
2021-08-19 20:43:34 +0100

    Rain

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -2,5 +2,18 @@
 """
 Rain
 """
-def rain(walls):
-	pass
+def rain(walls: int) -> int:
+    """calculation of rains"""
+    if not walls:
+        return 0
+    walls_length = len(walls)
+    water = 0
+    for i in range(walls_length):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
+    return water
