5828d43d403251f2f8a35f51537bd686b71c6f63
onsjannet <onsjannet@gmail.com>
2021-08-26 17:15:41 +0100

    betty

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -7,4 +7,4 @@ void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void heapify(int *arr, int index, int size, int len);
 void swap(int *a, int *b);
-#endif
+#endif /* SORT_H */
