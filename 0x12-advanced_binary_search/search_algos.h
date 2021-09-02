cc02e82f4f67c2f4bd166d0fb0f089f85ff4b6e3
onsjannet <onsjannet@gmail.com>
2021-09-02 08:29:45 +0100

    binary search algo

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+int advanced_binary(int *array, size_t size, int value);
+int recursiveBinarySearch(int array[], int start_index, int end_index, int element);
+void print_array(int *array, int start_index, int end_index);
+#endif /* SEARCH_ALGOS_H */
