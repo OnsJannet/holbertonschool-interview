4d1dbd6d27fb20bb7441c5faffb7500b21dc2e3e
onsjannet <onsjannet@gmail.com>
2021-09-30 10:07:06 +0100

    [Rotation of a 2D matrix]

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Rotate 2D Matrix
+'''
+import numpy as np
+
+
+def rotate_2d_matrix(matrix):
+    '''rotates 2D matrix 90° clockwise
+    '''
+    copied_matrix = matrix.copy()
+    reverced_copy = copied_matrix[::-1]
+    for elements in range(len(matrix[0])):
+        new_matrix = []
+        for row in reverced_copy:
+            new_matrix.append(row[elements])
+            matrix[elements] = new_matrix
