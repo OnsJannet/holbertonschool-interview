9164e702d08679b982310fcb36f17abce700d711
onsjannet <onsjannet@gmail.com>
2021-11-18 18:50:35 +0100

    island_perimeter

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+Island Perimeter
+'''
+from typing import List
+
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    '''
+        returns the perimeter of the island described in grid
+        grid is a list of list of integers
+        0 represents water
+        1 represents land
+
+            Find land (1).
+            Find neighbor (<->).
+            Return 4 * land -2 * neightbor.
+    '''
+    row, col = len(grid), len(grid[0])
+    land, neighbor_land = 0, 0
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                land += 1
+                if i < row-1 and grid[i+1][j] == 1:
+                    neighbor_land += 1
+                if j < col-1 and grid[i][j+1] == 1:
+                    neighbor_land += 1
+    return land*4-2*neighbor_land
